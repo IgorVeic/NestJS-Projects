4ffd8fdd97eb6c736218b6d3736186262773370c
Igor Veic <149272013+IgorVeic@users.noreply.github.com>
2024-09-15 10:18:38 +0200

    changes

--- music-app/swaggify/src/albums/dtos/album-response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { AlbumCreateDto } from "./album-create.dto";
-
-export class AlbumResponseDto extends AlbumCreateDto {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    }
--- music-app/swaggify/src/types/current-user.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from '../common/enums/role.enum';
-
-export interface ICurrentUser {
-  userId: string;
-  role: Role;
-  username: string;
-}
